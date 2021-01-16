1f902619d4552ae2cb57f9a464f742f05617b235
Priyanshu <Priyanshukaushal878@gmail.com>
2021-01-16 21:27:54 +0530

    Converted to Js files

---
+++ JSfile/Experience.js
@@ -0,0 +1,54 @@
+// main Container as data will added
+var experienceContainer = document.querySelector('.Experience')
+
+// array containig data Card view
+var experienceCard = [
+    {
+        title: 'Web-Developer Intern | careerguide.com',
+        desc:'',
+        date: '10-10-2020 - 07-12-2020 ',
+        Tool: 'Tool Used: Wordpress'
+    },
+    {
+        title: 'Web Developer | Verzeo',
+        desc:'',
+        date: '1-12-2019 - 31-1-2020 ',
+        Tool: 'Tool Used: HTML, CSS, JS, Django'
+    },
+    {
+        title: 'Hacktoberfest | Open Source | 2020',
+        desc:'Successful 4 PR ',
+        date: '',
+        Tool: ''
+    },
+    {
+        title: 'GeeksforGeeks | Open Source Competition | 2020',
+        desc:'Third Position @ Game of Source all over India',
+        date: '',
+        Tool: ''
+    },
+
+]
+
+function displaycard(){
+    console.log('ok');
+    let output = ""
+    experienceCard.forEach(({title ,desc,date, Tool}) =>{
+        output += `<div class="xl:w-1/2 md:w-1/2 p-4 ">
+        <div class="border border-gray-300 p-6 rounded-lg hover:bg-green-500 transition duration-500 ease-in-out  transform hover:-translate-y-1 hover:scale-11">
+          <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+              <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1zM4 22v-7"></path>
+            </svg>
+          </div>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-2">${title}</h2>
+          <p class="leading-relaxed text-base">${desc}${date}<br>${Tool}</p>
+        </div>
+      </div>`
+    
+    })
+    //  adding content to the container
+    experienceContainer.innerHTML = output
+}
+
+document.addEventListener("DOMContentLoaded", displaycard);
---
+++ JSfile/Skill.js
@@ -0,0 +1,56 @@
+var skillContainer = document.querySelector('.Skillcard')
+
+// Adding array to collect data
+
+var skillcard = [
+    
+    {
+        title:'C++',
+        img:'assest/cplus.png'
+    },
+
+    {
+        title:'Python',
+        img:'assest/python.png'
+    },
+    
+    {
+        title:'React',
+        img:'assest/react.png'
+    },
+    
+    {
+        title:'Django',
+        img:'assest/django-logo.png'
+    },
+    
+    {
+        title:'Web-Development',
+        img:'assest/html_css_js.png'
+    },
+    
+    {
+        title:'Open-Source Contributation',
+        img:'assest/github_C.svg'
+    },
+    
+]
+
+function displaycard(){
+    console.log('ok');
+    let output = ""
+    skillcard.forEach(({title, img}) =>{
+        output += `<div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+          <img alt="c++" class="w-16 h-16 object-cover object-center flex-shrink-0 rounded-full mr-4" src="${img}">
+          <div class="flex-grow">
+            <h2 class="text-white title-font font-medium">${title}</h2>
+          </div>
+        </div>
+      </div>`
+    })
+
+    skillContainer.innerHTML = output
+}
+
+document.addEventListener("DOMContentLoaded", displaycard);

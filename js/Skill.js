c8c47ccf6c71f486287d5342db628288a5870fbc
Priyanshu <Priyanshukaushal878@gmail.com>
2021-01-18 17:25:55 +0530

    Update

--- js/Skill.js
+++ js/Skill.js
@@ -37,12 +37,12 @@ var skillcard = [
 ]
 
 function displaycard(){
-    console.log('ok');
+    console.log('running');
     let output = ""
     skillcard.forEach(({title, img}) =>{
-        output += `<div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+        output += `<div class="p-2 lg:w-1/2 md:w-1/2 w-full">
         <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-          <img alt="c++" class="w-16 h-16 object-cover object-center flex-shrink-0 rounded-full mr-4" src="${img}">
+          <img class="w-16 h-16 object-cover object-center flex-shrink-0 rounded-full mr-4" src="${img}">
           <div class="flex-grow">
             <h2 class="text-white title-font font-medium">${title}</h2>
           </div>
---
+++ js/about.js
@@ -0,0 +1,46 @@
+var aboutContainer = document.querySelector('.about')
+
+var aboutcontent = [
+    {
+        intro:'I am learner while doing things in a Practical Way and Open Source Enthusiast.I am keen and very willing to learn and develop new skills.  Seeking a challenging position in a growth-oriented environment  where I can expand and utilise my learning and skills so that I can contribute towards organisation goals and establish a career for myself.',
+   
+    }
+]
+
+function displayAbout(){
+    console.log('running');
+    let output = ''
+    aboutcontent.forEach(({intro}) => {
+        output += `<div class="container px-5 py-24 mx-auto flex flex-wrap">
+    <div class="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-full overflow-hidden">
+      <img alt="feature" class="object-cover object-center" src="./assest/giphy.gif">
+    </div>
+    <div class="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
+      <div class="flex flex-col mb-10 lg:items-start items-center">
+        <div class="flex-grow">
+          <h1 class="title-font sm:text-2xl text-3xl text-white text-lg title-font font-medium text-center">About</h1>
+          <h1 class="title-font sm:text-4xl text-3xl text-green-700 text-lg title-font font-medium text-center mb-4">Let me introduce myself</h1>
+          <p class="leading-relaxed text-base text-justify text-white" style="font-size: x-large;">${intro}</p>
+        </div>
+      </div>
+      <h1 class="title-font sm:text-2xl text-3xl text-white text-lg title-font font-medium text-center border-b rounded-lg border-blue-500">Tools Which I Use </h1>
+      <div class="flex justify-center mt-8">
+        <a class="inline-flex focus:outline-none"><img src="./assest/wind_linux.png" alt="github" style="height:90px;"></a>
+
+        <a class="inline-flex focus:outline-none"><img src="./assest/vs_code.png" alt="Linkdin" style="height:80px;"></a>
+        
+        <a class="ml-4 inline-flex focus:outline-none "><img src="./assest/Github_colord.png" alt="Instagram" style="height:70px;"></a>
+        
+        <a class="ml-4 inline-flex focus:outline-none "><img src="./assest/slack.png" alt="Twitter" style="height:70px;"></a>
+
+        <a class="ml-4 inline-flex  focus:outline-none "><img src="./assest/discord.png" alt="Twitter" style="height:80px;"></a>
+      </div>
+    </div>
+  </div>`
+    })
+    
+
+  aboutContainer.innerHTML = output
+}
+
+document.addEventListener('DOMContentLoaded', displayAbout);
